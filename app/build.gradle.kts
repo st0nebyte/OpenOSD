8c25a1916802d91c06fe83ddbdd6470fe611d9f3
Fabian Steinbeißer <f.steinbeisser@vogl-electronic.com>
2026-03-10 13:39:35 +0100

    Bump version to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.st0nebyte.openosd"
         minSdk        = 22
         targetSdk     = 36
-        versionCode   = 5
-        versionName   = "0.4.0"
+        versionCode   = 6
+        versionName   = "0.4.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
